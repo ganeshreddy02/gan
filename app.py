46a1a41c67615649a1651376f293bb925fcf0baa
root <root@ip-172-31-25-50.ec2.internal>
2025-11-07 14:55:44 +0000

    hello

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 sum_result = a + b
 print(f"The sum of {a} and {b} is {sum_result}")
+<<<<<<< Updated upstream
 print('thank you')
 
+=======
+nk you
+>>>>>>> Stashed changes
